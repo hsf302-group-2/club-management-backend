19709ecc0b2a127e55ec527842fc14508d90fda9
phamquan1301 <pquan3278@gmail.com>
2025-07-09 14:52:08 +0700

    start
    # Conflicts:
    #	README.md

---
+++ src/main/java/com/hsf302_group2/club_management_system/ClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hsf302_group2.club_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hsf302_group2/club_management_system/ClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hsf302_group2.club_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
