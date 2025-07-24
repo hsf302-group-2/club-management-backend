23189b617f4a99ddf9a04e2c1cf094788d824396
phamquan1301 <pquan3278@gmail.com>
2025-07-24 08:56:18 +0700

    done

--- src/main/java/com/hsf302_group2/club_management_system/clubmember/repository/ClubMemberRepository.java
+++ src/main/java/com/hsf302_group2/club_management_system/clubmember/repository/ClubMemberRepository.java
@@ -1,14 +1,23 @@
 package com.hsf302_group2.club_management_system.clubmember.repository;
 
 import com.hsf302_group2.club_management_system.clubmember.entity.ClubMember;
+import com.hsf302_group2.club_management_system.premember.dto.response.PreMemberResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Repository
 public interface ClubMemberRepository extends JpaRepository<ClubMember, String> {
+    @Query("""
+        SELECT new com.hsf302_group2.club_management_system.premember.dto.response.PreMemberResponse (
+            p.id, u.id, u.email, u.fullName, u.status, p.gender, p.address, p.phoneNumber, p.dob
+            )
+        FROM PreMember p JOIN p.user u WHERE u.email = :email""")
+    Optional<PreMemberResponse> findPreMemberByEmail(@Param("email") String email);
+
 
 }
